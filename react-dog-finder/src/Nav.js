0360357cd0f2a8d8d6480fefecd721692828aa00
Tim Birkmire <timbirkmire@yahoo.com>
2021-04-23 10:51:16 -0400

    Style nav bar

--- react-dog-finder/src/Nav.js
+++ react-dog-finder/src/Nav.js
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Nav.css';
 
 const Nav = ({ dogs }) => {
   return (
-    <nav>
-      {dogs.map((d) => (
-        <Link key={d.name} to={`/dogs/${d.name}`}>
-          {d.name}
-        </Link>
-      ))}
+    <nav className='Nav'>
+      <div className='Nav-items-container'>
+        <NavLink key='home' className='Nav-item' to={`/dogs`}>
+          Home
+        </NavLink>
+        {dogs.map((d) => (
+          <NavLink key={d.name} className='Nav-item' to={`/dogs/${d.name}`}>
+            {d.name}
+          </NavLink>
+        ))}
+      </div>
     </nav>
   );
 };
