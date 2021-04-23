c1ea2c1af2fbea8be684eb67e72bbbeec95bbf6f
Tim Birkmire <timbirkmire@yahoo.com>
2021-04-23 13:36:37 -0400

    Add main functionality for color-factory app

---
+++ react-color-factory/src/Color.js
@@ -0,0 +1,22 @@
+import { useParams, Redirect, Link } from 'react-router-dom';
+
+const Color = ({ colors }) => {
+  const { color } = useParams();
+
+  const foundColor = colors.find((c) => c.name === color);
+  if (!foundColor) {
+    return <Redirect to='/colors' />;
+  }
+
+  return (
+    <div className='Color'>
+      <h1>{`THIS IS ${foundColor.name.toUpperCase()}`}</h1>
+      <h1>ISN'T IT BEAUTIFUL?</h1>
+      <Link to='/colors'>
+        <h1>GO BACK</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Color;
