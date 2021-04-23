a3c77b0a520c98b3596c3834bcbc63925b6fe812
Tim Birkmire <timbirkmire@yahoo.com>
2021-04-23 11:22:49 -0400

    Add styling for Dogs component

--- react-dog-finder/src/Dogs.js
+++ react-dog-finder/src/Dogs.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import './Dogs.css';
+
 const Dogs = ({ dogs }) => {
   return (
     <div className='Dogs'>
       {dogs.map((dog) => {
         return (
           <div className='Dogs-card'>
-            <h4 className='Dogs-name'>{dog.name}</h4>
+            <h3 className='Dogs-name'>{dog.name}</h3>
             <img className='Dogs-img' src={dog.src} alt={dog.name} />
             <Link to={`/dogs/${dog.name}`}>
               <button className='Dogs-btn'>Details</button>
